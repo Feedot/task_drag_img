53bdff2121acb5ddce639206f2607f0fb51db15b
1 <a1@MacBook-Air-1.lan>
2017-08-28 12:55:12 +0300

    hi

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,49 @@
-class DragAndDrop{
-    constructor(elemId){
+class DragAndDrop {
+
+    constructor (elemId) {
+
         this.elem = document.getElementById(elemId);
         this.pElem = document.querySelector('p');
-        this.elem.ondragover = ()=> this.changeClassName('hover');
-        this.elem.ondragleave = ()=> this.changeClassName('');
+        this.elem.ondragover = () => this.changeClassName('hover');
+        this.elem.ondragleave = () => this.changeClassName('');
         this.elem.ondrop = e => this.createImgList(e);
+
     }
-    changeClassName(className){
+    changeClassName(className) {
+
         this.elem.className = className;
         return false
+
     }
-    changePElem(className,innerHtml){
+    changePElem(className,innerHtml) {
+
         this.pElem.className = className;
         this.pElem.innerHTML = innerHtml
+
     }
-    createImgList(e){
+    createImgList(e) {
+
         e.preventDefault();
         this.elem.className = '';
         this.changePElem('','Success! Pull one more image..');
-        Array.from(e.dataTransfer.files).map(file=> {
+
+        Array.from(e.dataTransfer.files).map(file => {
+
             if (~file.type.indexOf('image')) {
+
                 let fread = new FileReader();
                 fread.readAsDataURL(file);
-                fread.onload = e =>{
+
+                fread.onload = e => {
                         let img = new Image();
                         img.src = e.target.result;
                         document.body.appendChild(img);
                     };
+
             } else this.changePElem('error','Error!! Pull only image....')
+
         })
     }
 
 }
-window.onload = () =>{const drag = new DragAndDrop('dropZone')}
+window.onload = () => new DragAndDrop('dropZone');
